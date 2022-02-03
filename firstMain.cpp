0782e5c093d633865ddb9f60f6cf1232ca3d4e19
beddeb <98533141+beddeb@users.noreply.github.com>
2022-02-03 17:03:18 +0300

    03.02.2022

---
+++ firstMain.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    w.resize(500, 500);
+    w.setWindowTitle("Test Menu");
+
+    w.show();
+    return a.exec();
+}
